0c0ceee5d5e32a1d2aeb3cce8bb87159230eea45
Matthijs4004 <matthijs4004@gmail.com>
2022-03-21 10:53:56 +0100

    refresh function af

--- read-registration.py
+++ read-registration.py
@@ -1,5 +1,7 @@
+import os,json
 import tkinter as tk
 from tkinter import ttk
+from datetime import datetime
 
 window = tk.Tk()
 window.title("Read Registration")
@@ -7,7 +9,23 @@ window.geometry("820x300")
 latestVar = tk.StringVar()
 
 def refresh():
-    pass
+    global latestVar
+    
+    mainFile = os.listdir(r"C:\Projecten\Software Dev\Mapje 9\file-communicate\databron/")
+    fileList = []
+    latest = 0
+    for filename in mainFile:
+        file = open(r"C:\Projecten\Software Dev\Mapje 9\file-communicate\databron/"+filename, "r")
+        fileRead = file.read()
+        jsonString = json.loads(fileRead)
+        fileList.append((jsonString["Naam"],jsonString["Email"], jsonString["Geboorte"], jsonString["Geslacht"]))
+        latest = max(latest, os.path.getmtime(r"C:\Projecten\Software Dev\Mapje 9\file-communicate\databron/"+filename))
+
+    register.delete(*register.get_children())    
+    for files in fileList:
+        register.insert('', tk.END, values=files)
+    
+    latestVar.set("Laatste Aanpassing: "+str(datetime.fromtimestamp(latest).strftime("%A, %B %d, %Y %I:%M:%S")))
 
 columns = ("Naam", "Email" ,"Geboorte", "Geslacht")
 
@@ -28,6 +46,6 @@ btn.grid(row=2,column=0, sticky='ns')
 latest = ttk.Label(window, textvariable=latestVar)
 latest.grid(row=3,column=0)
 
-#refresh()
+refresh()
 
 window.mainloop()
